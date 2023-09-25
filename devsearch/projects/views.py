9c9a94599b591a5b12637c9b6504bdda3bb0fe95
Smith0988 <chungdv.mta@gmail.com>
2023-09-25 17:40:54 +0700

    training

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+project_List = [
+    {
+        'id': '1',
+        'title': "Ecom WWebsite",
+        'description': 'Fully functional ecom website',\
+    },
+    {
+        'id': '2',
+        'title': "Portfolio Website",
+        'description': 'Fully functional ecom website'
+    },
+    {
+        'id': '3',
+        'title': "Social Network Website",
+        'description': 'Awesome open source project'
+
+    },
+
+]
+
 
 def projects(request):
-    return render(request, 'projects.html')
+    page = 'projects'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': project_List}
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    return render(request, 'single-project.html')
+    project_obj = None
+    for i in project_List:
+        if i['id'] == pk:
+            project_obj = i
+    return render(request, 'projects/single-project.html', {'project': project_obj})
